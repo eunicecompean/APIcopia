d0b6a93bbff42c63b29ab58a1471f149bbc4e63f
Julia <maydeli.castan@iest.edu.mx>
2025-11-05 18:06:52 -0600

    Initial Commit

---
+++ API/APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIApp.swift
+//  API
+//
+//  Created by WIN603 on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
